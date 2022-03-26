78470b7cabfc5ec266ceb9788c0dd88b8c968b36
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-26 22:31:50 +0530

    Update testing.js

--- src/testing.js
+++ src/testing.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+/*********
+ * Testing
+ */
+const data = require('./module.js'),
+	booru = new data.booru();
+booru.setSite({
+	site: 'sb'
+});
+booru.search({
+	query: ['girl']
+}).then((r) => {
+	console.log(r)
+});
